0752edcb530c5ce0aab4f0917c061293a62453aa
git-chad <tobimocc@gmail.com>
2024-02-29 14:28:55 -0300

    fix(dev): apos

--- components/Scene.jsx
+++ components/Scene.jsx
@@ -8,7 +8,7 @@ const Scene = () => {
   return (
     // backgroundImage: 'linear-gradient(to bottom, #2d3748, #1a202c)'
     <Canvas style={{ backgroundColor: "transparent" }}>
-      <Text fontSize={1.05} font="/Geist-Black.otf" position={[0, 0, -0.95]}>Don't drink</Text>
+      <Text fontSize={1.05} font="/Geist-Black.otf" position={[0, 0, -0.95]}>Don&apos;t drink</Text>
       <Text fontSize={0.8} font="/Geist-Black.otf" position={[0, -0.8, -0.95]}>and drive</Text>
       <directionalLight intensity={3} position={[0, 3, 2]} />
       <Environment preset="city" />
